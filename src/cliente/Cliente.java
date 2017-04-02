cec740237d6d841e0a5a4b1f1cd903657cdfbb03
Filipe <filipe.goncalves@uniriotec.br>
2017-04-02 13:38:37 -0300

    Commit inicial Já consome os serviços de multiplicação e consulta por publicação

---
+++ src/cliente/Cliente.java
@@ -0,0 +1,40 @@
+package cliente;
+
+import unirio.Publicacoes;
+
+
+
+/**
+ *
+ * @author Filipe-pc
+ */
+public class Cliente {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //unirio.Publicacoes pub = consultaPorNome("artigo");
+        System.out.println("Multiplicacao: " + multiplicacao(3, 12));
+
+        Publicacoes pub = consultaPorNome("1");
+        for (int i = 0; i < pub.getPublicacoes().size(); i++) {
+            System.out.println(pub.getPublicacoes().get(i).getTitulo());
+        }
+
+    }
+
+    private static Publicacoes consultaPorNome(String nomePublicacao) {
+       unirio.Servico_Service servico = new unirio.Servico_Service();
+        
+        unirio.Servico port = servico.getServicoPort();
+        return port.consultaPorNome(nomePublicacao);
+    }
+
+    private static int multiplicacao(int inteiro1, int inteiro2) {
+        unirio.Servico_Service service = new unirio.Servico_Service();
+        unirio.Servico port = service.getServicoPort();
+        return port.multiplicacao(inteiro1, inteiro2);
+    }
+
+}
