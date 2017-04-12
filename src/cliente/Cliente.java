1e57259bf08cc55f6071ab8c6e71842adabfc3af
Filipe <filipe.goncalves@uniriotec.br>
2017-04-12 19:56:12 -0300

    Serviço de delete adicionado

--- src/cliente/Cliente.java
+++ src/cliente/Cliente.java
@@ -1,8 +1,7 @@
 package cliente;
 
+import java.util.Scanner;
 import unirio.Publicacoes;
-
-
 
 /**
  *
@@ -11,31 +10,45 @@ import unirio.Publicacoes;
 public class Cliente {
 
     public static void main(String[] args) {
-        //unirio.Publicacoes pub = consultaPorNome("artigo");
-        System.out.println("Multiplicacao: " + multiplicacao(3, 12));
-        
-        System.out.println("Resultado add " + addPublicacao(9, "Artigo 9", 3, 30, 1990));
-        
-        
 
-        Publicacoes pub = consultaPublicacaoPorNome("Artigo 9");
+        Scanner scan = new Scanner(System.in);
+        int entrada = -1;
+
+        System.out.println("Multiplicacao: " + multiplicacao(3, 12));
+
+        Publicacoes pub = consultaPublicacaoPorNome("");
         for (int i = 0; i < pub.getPublicacoes().size(); i++) {
-            System.out.println(pub.getPublicacoes().get(i).getTitulo() 
-                    +" Ano de publicação: "+ pub.getPublicacoes().get(i).getAnoPublicacao());
+
+            System.out.println(pub.getPublicacoes().get(i).getId() + " - " + pub.getPublicacoes().get(i).getTitulo()
+                    + " Ano de publicação: " + pub.getPublicacoes().get(i).getAnoPublicacao());
+        }
+
+        while (entrada != 0) {
+           
+            System.out.println("Digite 0 para sair ou o numero da publicação que deseja eliminar");
+            entrada = scan.nextInt();
+            removePublicacao(entrada);
+        }
+        
+        pub = consultaPublicacaoPorNome("");
+        for (int i = 0; i < pub.getPublicacoes().size(); i++) {
+
+            System.out.println(pub.getPublicacoes().get(i).getTitulo()
+                    + " Ano de publicação: " + pub.getPublicacoes().get(i).getAnoPublicacao());
         }
 
     }
 
     private static Publicacoes consultaPorNome(String nomePublicacao) {
-       unirio.Servico_Service servico = new unirio.Servico_Service();
-        
+        unirio.Servico_Service servico = new unirio.Servico_Service();
+
         unirio.Servico port = servico.getServicoPort();
         return port.consultaPorNome(nomePublicacao);
     }
-    
-     private static Publicacoes consultaPublicacaoPorNome(String nomePublicacao) {
-       unirio.Servico_Service servico = new unirio.Servico_Service();
-        
+
+    private static Publicacoes consultaPublicacaoPorNome(String nomePublicacao) {
+        unirio.Servico_Service servico = new unirio.Servico_Service();
+
         unirio.Servico port = servico.getServicoPort();
         return port.consultaPublicacaoPorNome(nomePublicacao);
     }
@@ -46,10 +59,16 @@ public class Cliente {
         return port.multiplicacao(inteiro1, inteiro2);
     }
 
-    private static Boolean addPublicacao(int Id, String titulo, int paginaInicial, int paginaFinal,Integer  anoPublicacao) {
-       unirio.Servico_Service service = new unirio.Servico_Service();
+    private static Boolean addPublicacao(int Id, String titulo, int paginaInicial, int paginaFinal, Integer anoPublicacao) {
+        unirio.Servico_Service service = new unirio.Servico_Service();
         unirio.Servico port = service.getServicoPort();
-        return port.addPublicacao( Id,  titulo, paginaInicial, paginaFinal, anoPublicacao);
+        return port.addPublicacao(Id, titulo, paginaInicial, paginaFinal, anoPublicacao);
+    }
+
+    private static Boolean removePublicacao(int Id) {
+        unirio.Servico_Service service = new unirio.Servico_Service();
+        unirio.Servico port = service.getServicoPort();
+        return port.removePublicacao(Id);
     }
 
 }
